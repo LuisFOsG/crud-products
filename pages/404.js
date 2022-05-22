001cb262932418021835ab5d3fe82fcfd757847d
LuisFOsG <luisfosgdev@gmail.com>
2022-05-21 19:11:57 -0500

    feat: add initial page

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+export default function Products() {
+  return (
+    <div>
+      <h1>Página no Encontrada</h1>
+    </div>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
-
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const color = "white";
+
+  return <>
+    <Component {...pageProps} />
+
+    <style global jsx>{`
+      html,
+      body {
+        width: 100%;
+        height: 100%;
+
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        background-color: ${color};
+      }
+
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
+
+      * {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+      }
+
+    `}</style>
+  </>
 }
 
 export default MyApp
