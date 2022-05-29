cbb49cd033477927969b7b18e5ee41eeef435b35
LuisFOsG <luisfosgdev@gmail.com>
2022-05-29 15:19:24 -0500

    feat(Root): add preview product

--- components/Expanded.js
+++ components/Expanded.js
@@ -8,43 +8,76 @@ const coLocale = Intl.NumberFormat('es-CO', {
 const ExpandedComponent = ({ data }) => {
   return (
     <>
-      <div className="card">
-        <div className="wrap-image">
-          { data.image && <Image layout="fill" objectFit='contain' src={data.image} alt={data.name} /> }
+      <div className="container">
+        <div className='wrap-image' >
+          <Image layout="fill" objectFit='contain' src={data.image} alt={data.name} />
         </div>
-        <div className="card-content">
-          <h3>{ data.name }</h3>
+        <div className='content' >
+          <h1>{data.name}</h1>
           <small>{ coLocale.format(data.price) }</small>
-          <p>{ data.description.substring(0, 100).trim() }...</p>
+          <p className='description'>{data.description}{ !data.description.endsWith('.') && '.' }</p>
+
+          <article>
+            <h2>Cantidades Disponibles</h2>
+            <p className="option">{ data.quantity }</p>
+            <h2>Via Administración</h2>
+            <p className="option">{ data.viaAdministration }</p>
+            <h2>Concentración</h2>
+            <p className="option">{ data.concentration }</p>
+            <h2>Forma Farmacéutica</h2>
+            <p className="option">{ data.pharmaceuticalForm }</p>
+          </article>
         </div>
       </div>
 
       <style jsx>{`
-        .card {
-          background: var(--bg-color);
+        .container {
           width: 100vw;
-
           display: flex;
-          flex-direction: column;
+          flex-direction: row;
+          justify-content: center;
+          align-items: center;
 
-          color: var(--primary-color);
-          padding: 2rem;
-          cursor: pointer;
+          gap: 2rem;
+          padding: 0 4rem;
         }
 
         .wrap-image {
           position: relative;
-          width: 100%;
-          height: 200px;
+          width: 40%;
+          height: 500px;
         }
 
-        h3 {
+        .content {
+          width: 60%;
+          padding: 2rem;
+          border-radius: 15px;
+          background-color: var(--bg-color-opacity);
+          color: var(--primary-color);
+        }
+
+        .description {
+          margin-top: 2rem;
+        }
+
+        article {
+          margin-top: 2rem;
+        }
+
+        h1 {
           text-align: center;
+          font-size: 2rem;
+          font-weight: bold;
+          margin-bottom: 2rem;
         }
 
-        p {
-          margin-top: 1rem;
-          opacity: 0.7;
+        h2 {
+          padding-left: 1rem;
+        }
+
+        .option {
+          padding-left: 2rem;
+          margin-bottom: 1rem;
         }
       `}</style>
     </>
